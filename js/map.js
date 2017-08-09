f0664348f104ba196674320f58f16fd8d3954bb8
GaganTut <gstut@live.com>
2017-08-09 09:56:28 -0700

    Changed default map color and added colors to countries that charles has visited

--- js/map.js
+++ js/map.js
@@ -1,8 +1,37 @@
-
 var map = new Datamap({
   element: document.getElementById('map'),
   geographyConfig: {
     highlightOnHover: false,
     popupOnHover: true
+  },
+  fills: {
+    defaultFill: '#D3D3D3',
+    traveledTo: '#0072bc'
+  },
+  data: {
+    USA: {
+      fillKey: 'traveledTo'
+    },
+    MEX: {
+      fillKey: 'traveledTo'
+    },
+    BEL: {
+      fillKey: 'traveledTo'
+    },
+    COD: {
+      fillKey: 'traveledTo'
+    },
+    FRA: {
+      fillKey: 'traveledTo'
+    },
+    DEU: {
+      fillKey: 'traveledTo'
+    },
+    CHN: {
+      fillKey: 'traveledTo'
+    },
+    SGP: {
+      fillKey: 'traveledTo'
+    }
   }
 });
